32861604ac0f79d495dfb2534b2987d06065b856
Promphong Pantuwon <p.promphong@gmail.com>
2018-11-21 17:57:10 +0700

    Add files via upload

--- Mkt_timing_model.py
+++ Mkt_timing_model.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 from keras.layers import Input, Dense, LSTM
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras.optimizers import Adam
 from keras.utils import to_categorical
 from keras import regularizers #Improve wight update avoid overfit
@@ -32,25 +32,29 @@ def wavelet_processing(df, year_train, last_col, lookback):
     for i in range(len(df)):
         train = []
         for j in range(0, last_col):
-            x = np.array(df.iloc[i:i+lookback+1, j])
-            (ca, cd) = pywt.dwt(x, "haar")
-            cat = pywt.threshold(ca, np.std(ca), mode="soft")
-            cdt = pywt.threshold(cd, np.std(cd), mode="soft")
-            tx = pywt.idwt(cat, cdt, "haar")
-            log_tx = np.log(tx)
-            train = np.append(train, log_tx)
+            x = np.array(df.iloc[i:i+lookback, j])
+#            (ca, cd) = pywt.dwt(x, "haar", 'smooth')
+#            #cat = pywt.threshold(ca, np.std(ca), mode="soft")
+#            #cdt = pywt.threshold(cd, np.std(cd), mode="soft")
+#            tx = pywt.idwt(ca, cd, "haar")
+            diff_tx = np.diff(np.log(x))
+            train = np.append(train, diff_tx)
         train_data.append(train)
     trained = pd.DataFrame(train_data, index=None)
 
     pre_en_train = pd.DataFrame(trained[0:train_data_range], index=None)
-    pre_en_test = pd.DataFrame(trained[train_data_range+1:train_data_range+test_data_range+1], index=None)
+#    pre_en_train = pre_en_train.shift(-1)
+    pre_en_train = pre_en_train.fillna(0)
+    pre_en_test = pd.DataFrame(trained[train_data_range+1:train_data_range+test_data_range], index=None)
+    pre_en_test = pre_en_test.fillna(0)
     
 #    for i in range(len(df)):
 #        y = df.iloc[i+lookback+1, 1] / df.iloc[i+lookback, 1]
 #        label.append(y)
-    log_re = np.where(np.diff(np.log(df.iloc[:,0])) > 0, 1, -1)
+#    log_re = np.where(np.diff(np.log(df.iloc[:,0])) > 0, 1, -1)
+    log_re = np.diff(np.log(df.iloc[:, 0]))
     label_train = pd.DataFrame(log_re[0:train_data_range], index=None)
-    label_test = pd.DataFrame(log_re[train_data_range:train_data_range+test_data_range], index=None)
+    label_test = pd.DataFrame(log_re[train_data_range+1:train_data_range+test_data_range], index=None)
     return pre_en_train, pre_en_test, label_train, label_test
     
 #TODO: 3) Pre-Processing Data (Autoencoder) self supervise
@@ -61,16 +65,15 @@ def autoencoder_processing(data, ratio, encoded_dim, input_shape,
     input_data = Input(shape=(1, input_shape))
     encoded1 = Dense(encoded1_shape, activation="relu",activity_regularizer=regularizers.l2(10e-5))(input_data)
     encoded2 = Dense(encoded2_shape, activation="relu",activity_regularizer=regularizers.l2(10e-5))(encoded1)
-    encoded3 = Dense(encoded_dim, activation="relu",activity_regularizer=regularizers.l2(10e-5))(encoded2)
+    encoded3 = Dense(encoded_dim, activation="tanh",activity_regularizer=regularizers.l2(10e-5))(encoded2)
     decoded1 = Dense(decoded1_shape, activation="relu",activity_regularizer=regularizers.l2(10e-5))(encoded3)
     decoded2 = Dense(decoded2_shape, activation="relu",activity_regularizer=regularizers.l2(10e-5))(decoded1)
-    decoded = Dense(input_shape, activation="sigmoid")(decoded2)
+    decoded = Dense(input_shape, activation="tanh")(decoded2)
     autoencoder = Model(inputs=input_data, outputs=decoded) # Evaluate  
     encoder = Model(input_data, encoded3) # use this model compress data to LSTM
     
     # Manage Data
-    scaler = MinMaxScaler(feature_range=(-1,1))
-    scaled_raw = scaler.fit_transform(data.values)
+    scaled_raw = data.values
     scaled_data = np.reshape(scaled_raw, (len(data), 1, input_shape))
     train_data = scaled_data[0:int(len(data)*ratio)]
     test_data = scaled_data[int(len(data)*ratio)+1:]
@@ -78,10 +81,10 @@ def autoencoder_processing(data, ratio, encoded_dim, input_shape,
     # Train model
     #cutom_adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.9, decay=0.0) 
     autoencoder.compile(loss='mean_absolute_error', optimizer='adam')
-    history = autoencoder.fit(train_data, train_data, epochs=100,
+    autoencoder.fit(train_data, train_data, epochs=100,
                     validation_data=(test_data, test_data))
 #    print(autoencoder.evaluate(test_data, test_data))
-    plt.plot(history.history['loss'])
+#    plt.plot(history.history['loss'])
 #    plt.plot(history.history['val_loss'])
     # Use encode generate encode data before LSTM
     coded_train = []
@@ -92,50 +95,63 @@ def autoencoder_processing(data, ratio, encoded_dim, input_shape,
         shaped = np.reshape(coded, (encoded_dim,))
         coded_train.append(shaped)
     
-    return pd.DataFrame(coded_train)
+    df_coded = pd.DataFrame(coded_train)
+#    coded_train = scaler.inverse_transform(np.array(coded_train))
+    
+    return df_coded
     
 #TODO: 3) Train LSTM Model
-def train_lstm_model(input_shape, train_data, train_label_data, test_data, test_label_data):
+def train_lstm_model(input_shape, train_data, train_label_data,
+                     test_data, test_label_data, predict_level=False):
     
-    # Construct LSTM
+    # Construct LSTM with Functional API
     input_data = Input(shape=(1, input_shape))
-    lstm = LSTM(5, input_shape=(1, input_shape), return_sequences=True,
-               recurrent_regularizer=regularizers.l2(0), dropout=0.2)(input_data)
-    perc = Dense(5, activation="sigmoid", activity_regularizer=regularizers.l2(0.005))(lstm)
-    lstm2 =LSTM(2, activity_regularizer=regularizers.l2(0.01),
-                dropout=0.2)(perc)
-    output = Dense(1, activation="sigmoid", activity_regularizer=regularizers.l2(0.001))(lstm2)
+    lstm = LSTM(5, input_shape=(1, input_shape), return_sequences=True, activity_regularizer=regularizers.l2(0.003),
+                   recurrent_regularizer=regularizers.l2(0), dropout=0.2)(input_data)
+    hidden1 = Dense(5,activation="tanh", activity_regularizer=regularizers.l2(0.005))(lstm)
+    lstm2 = LSTM(2, activity_regularizer=regularizers.l2(0.01),
+                    dropout=0.2, recurrent_dropout=0.2)(hidden1)
+#    hidden2 = Dense(2, activity_regularizer=regularizers.l2(0.001))(lstm2)
+    output = Dense(1, activation="tanh", activity_regularizer=regularizers.l2(0.001))(lstm2)
     model = Model(input_data, output)
-    model.compile(optimizer='adam', loss='mse', metrics=['mse'])
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    
+    # Construct LSTM with Sequential Constructor
+#    model = Sequential()
+#    model.add(LSTM(5, batch_input_shape=(1, 1, input_shape)))
+#    model.add(Dense(1))
+#    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
     
     # Manage Data to LSTM input [sample, step, feature]
     train_x = np.reshape(np.array(train_data), (train_data.shape[0], 1, train_data.shape[1]))
     train_y = np.array(train_label_data)
     test_x = np.reshape(np.array(test_data), (test_data.shape[0], 1, test_data.shape[1]))
     test_y = np.array(test_label_data)
-    # convert y to binary
-#    train_y = to_categorical(train_y)
-#    test_y = to_categorical(test_y)
     
     # Train model
-    model.fit(train_x, train_y, epochs=100, shuffle=False)
+    history = model.fit(train_x, train_y, epochs=10, shuffle=False)
+    
+    plt.plot(history.history['loss'])
+    print('Model Performance: ',model.evaluate(test_x, test_y))
     
     # Predict 
     prediction_data = []
+    prediction_level = []
     
     for i in range(len(test_y)):
         # Predict regression
         prediction = (model.predict(np.reshape(test_x[i], (1, 1, input_shape))))
         prediction_data.append(np.reshape(prediction, (1,)))
+        #level = np.exp(np.reshape(prediction, (1,))) * 
 #    plt.plot(prediction_corrected)
     
     plt.figure()
     plt.plot(prediction_data)
-    predict_class = np.where(np.array(prediction_data) > 0.5, 1, 0) 
+    #predict_class = np.where(np.array(prediction_data) > 0.5, 1, 0) 
     
-    return model, predict_class
+    return model, prediction_data
     
-#TODO: 4) Predict Value
+#TODO: 4) Alternative Model (Multivariate Seq-to-Seq Encode Decode)
 
     
 if __name__ == '__main__':
@@ -148,22 +164,38 @@ if __name__ == '__main__':
 #    raw_df = raw_df.join(macd)
 #    df = raw_df.dropna(inplace=True)
     
-    pre_en_train, pre_en_test, label_train, label_test = wavelet_processing(raw_df, 2010, 10, 5)
-    # Generate Autoencode X
-    encoded_train = autoencoder_processing(pre_en_train, ratio=0.8, encoded_dim=11,
-                                         input_shape=50, encoded1_shape=11,
-                                         encoded2_shape=11,
-                                         decoded1_shape=11,
-                                         decoded2_shape=11)
+    #for rolling sample 
+    #raw_df[(raw_df.index.year >=2010) & (raw_df.index.year <=2012)]
     
-    encoded_test = autoencoder_processing(pre_en_test, ratio=0.8, encoded_dim=11,
-                                     input_shape=50, encoded1_shape=11,
-                                     encoded2_shape=11,
-                                     decoded1_shape=11,
-                                     decoded2_shape=11)
+    # Denoise data and manipulate data
+    pre_en_train, pre_en_test, label_train, label_test = wavelet_processing(raw_df, 2010, 10 , 2)
+    
+#    print('-'*10, "Autoencoder Process", '-'*10)
+#    
+#    #Generate Autoencode X
+#    encoded_train = autoencoder_processing(pre_en_train, ratio=0.8, encoded_dim=10,
+#                                         input_shape=50, encoded1_shape=30,
+#                                         encoded2_shape=20,
+#                                         decoded1_shape=20,
+#                                         decoded2_shape=30)
+#    
+#    encoded_test = autoencoder_processing(pre_en_test, ratio=0.8, encoded_dim=10,
+#                                     input_shape=50, encoded1_shape=30,
+#                                     encoded2_shape=20,
+#                                     decoded1_shape=20,
+#                                     decoded2_shape=30)
+    
+    print('-'*10, "LSTM Process", '-'*10)
     
     # Train LSTM model 
-#    lstm_test, prediction = train_lstm_model(11, pre_en_train, label_train, pre_en_test, label_test)
+    lstm_test, prediction = train_lstm_model(10, pre_en_train, label_train, pre_en_test, label_test)
+    
+#    plt.subplot(211)
+    plt.plot(pd.DataFrame(prediction), label='LSTM Predict')
+#    plt.gca().set_title('Predict')
+#    plt.subplot(212)
+    plt.plot(label_test, label='Diff log ACWI')
+#    plt.gca().set_title('ACWI diff log')
     
     #df_train = raw_df[raw_df.index.year <= 2010]
     #df_test = raw_df[raw_df.index.year == 2011]
